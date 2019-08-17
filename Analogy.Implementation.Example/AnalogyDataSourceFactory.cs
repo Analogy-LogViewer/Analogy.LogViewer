5db120e0938736ce768879b00ee7282e3b86b2ac
Lior Banai <liorbanai@gmail.com>
2019-08-17 14:32:11 +0200

    Add Example project

---
+++ Analogy.Implementation.Example/AnalogyDataSourceFactory.cs
@@ -0,0 +1,12 @@
+﻿using Philips.Analogy.Interfaces.Interfaces;
+using System.Collections.Generic;
+
+namespace Analogy.Implementation.Example
+{
+    public class AnalogyDataSourceFactory : IAnalogyDataSourceFactory
+    {
+        public string Title => "Analogy Online example";
+
+        public IEnumerable<IAnalogyDataSource> Items => new List<IAnalogyDataSource> { new AnalogyOnlineExampleDataSource() };
+    }
+}
---
+++ Analogy.Implementation.Example/AnalogyExampleFactory.cs
@@ -0,0 +1,18 @@
+﻿using Philips.Analogy.Interfaces.Interfaces;
+using System;
+
+namespace Analogy.Implementation.Example
+{
+    public class AnalogyExampleFactory : IAnalogyFactories
+    {
+        public Guid FactoryID => new Guid("4B1EBC0F-64DD-44A1-BC27-79DBFC6384CC");
+
+        public string Title => "Analogy Examples";
+
+        public IAnalogyDataSourceFactory DataSources { get; } = new AnalogyDataSourceFactory();
+
+        public IAnalogyCustomActionFactory Actions => null;
+
+        public IAnalogyUserControlFactory UserControls => null;
+    }
+}
