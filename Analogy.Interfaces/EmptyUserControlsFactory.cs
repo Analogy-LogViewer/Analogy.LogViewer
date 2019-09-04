d1ef94154ed43910c51e3c37a8b2e4a082bf789d
Lior Banai <liorbanai@gmail.com>
2019-09-04 21:31:13 +0300

    adding empty default implementation for Factories that are not used.

---
+++ Analogy.Interfaces/EmptyUserControlsFactory.cs
@@ -0,0 +1,12 @@
+﻿using Philips.Analogy.Interfaces.Interfaces;
+using System.Collections.Generic;
+
+namespace Philips.Analogy.Interfaces
+{
+    public class EmptyUserControlsFactory : IAnalogyUserControlFactory
+    {
+        public string Title => "Empty User Controls";
+
+        public IEnumerable<IAnalogyUserControl> Items => new List<IAnalogyUserControl>(0);
+    }
+}
