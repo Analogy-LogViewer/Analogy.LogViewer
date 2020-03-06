e03a33118e4f1abc208b71f35f66f5c9bedc5a4e
Lior Banai <liorbanai@gmail.com>
2020-03-06 12:46:11 +0200

    update interface changes

--- Analogy.UnitTests/MessagePackFormatTests.cs
+++ Analogy.UnitTests/MessagePackFormatTests.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Analogy.Interfaces;
-using Analogy.LogLoaders;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Analogy.UnitTests
