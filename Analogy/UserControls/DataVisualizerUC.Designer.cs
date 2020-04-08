7e73417cb5852d04921e84372935c0a8a4fa537e
Lior Banai <liorbanai@gmail.com>
2020-04-08 23:34:12 +0300

    cleanup ui

--- Analogy/UserControls/DataVisualizerUC.Designer.cs
+++ Analogy/UserControls/DataVisualizerUC.Designer.cs
@@ -31,11 +31,7 @@
             DevExpress.XtraCharts.SideBySideBarSeriesLabel sideBySideBarSeriesLabel4 = new DevExpress.XtraCharts.SideBySideBarSeriesLabel();
             DevExpress.XtraCharts.SideBySideBarSeriesLabel sideBySideBarSeriesLabel5 = new DevExpress.XtraCharts.SideBySideBarSeriesLabel();
             DevExpress.XtraCharts.SideBySideBarSeriesLabel sideBySideBarSeriesLabel6 = new DevExpress.XtraCharts.SideBySideBarSeriesLabel();
-            this.splitterControl1 = new DevExpress.XtraEditors.SplitterControl();
             this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
-            this.listBoxControl1 = new DevExpress.XtraEditors.ListBoxControl();
-            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
-            this.nudTextLength = new System.Windows.Forms.NumericUpDown();
             this.sBtnAdd = new DevExpress.XtraEditors.SimpleButton();
             this.chklistItems = new DevExpress.XtraEditors.CheckedListBoxControl();
             this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
@@ -47,10 +43,12 @@
             this.chartControlFrequency = new DevExpress.XtraCharts.ChartControl();
             this.tnpTimeDistribution = new DevExpress.XtraBars.Navigation.TabNavigationPage();
             this.chartTimeDistribution = new DevExpress.XtraCharts.ChartControl();
+            this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
+            this.xtpMessagesText = new DevExpress.XtraTab.XtraTabPage();
+            this.xtpPieChart = new DevExpress.XtraTab.XtraTabPage();
+            this.scPies = new DevExpress.XtraEditors.SplitContainerControl();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
             this.splitContainerControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudTextLength)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chklistItems)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tabPane1)).BeginInit();
@@ -64,27 +62,22 @@
             this.tnpTimeDistribution.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartTimeDistribution)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(sideBySideBarSeriesLabel6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).BeginInit();
+            this.xtraTabControl1.SuspendLayout();
+            this.xtpMessagesText.SuspendLayout();
+            this.xtpPieChart.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scPies)).BeginInit();
+            this.scPies.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // splitterControl1
-            // 
-            this.splitterControl1.Location = new System.Drawing.Point(0, 0);
-            this.splitterControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.splitterControl1.Name = "splitterControl1";
-            this.splitterControl1.Size = new System.Drawing.Size(6, 757);
-            this.splitterControl1.TabIndex = 0;
-            this.splitterControl1.TabStop = false;
             // 
             // splitContainerControl1
             // 
             this.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl1.FixedPanel = DevExpress.XtraEditors.SplitFixedPanel.None;
             this.splitContainerControl1.Horizontal = false;
-            this.splitContainerControl1.Location = new System.Drawing.Point(6, 0);
+            this.splitContainerControl1.Location = new System.Drawing.Point(0, 0);
             this.splitContainerControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.splitContainerControl1.Name = "splitContainerControl1";
-            this.splitContainerControl1.Panel1.Controls.Add(this.listBoxControl1);
-            this.splitContainerControl1.Panel1.Controls.Add(this.labelControl2);
-            this.splitContainerControl1.Panel1.Controls.Add(this.nudTextLength);
             this.splitContainerControl1.Panel1.Controls.Add(this.sBtnAdd);
             this.splitContainerControl1.Panel1.Controls.Add(this.chklistItems);
             this.splitContainerControl1.Panel1.Controls.Add(this.textEdit1);
@@ -92,57 +85,15 @@
             this.splitContainerControl1.Panel1.Text = "Panel1";
             this.splitContainerControl1.Panel2.Controls.Add(this.tabPane1);
             this.splitContainerControl1.Panel2.Text = "Panel2";
-            this.splitContainerControl1.Size = new System.Drawing.Size(1324, 757);
-            this.splitContainerControl1.SplitterPosition = 194;
+            this.splitContainerControl1.Size = new System.Drawing.Size(1008, 537);
+            this.splitContainerControl1.SplitterPosition = 138;
             this.splitContainerControl1.TabIndex = 1;
             this.splitContainerControl1.Text = "splitContainerControl1";
-            // 
-            // listBoxControl1
-            // 
-            this.listBoxControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.listBoxControl1.HorizontalScrollbar = true;
-            this.listBoxControl1.Location = new System.Drawing.Point(839, 74);
-            this.listBoxControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.listBoxControl1.Name = "listBoxControl1";
-            this.listBoxControl1.Size = new System.Drawing.Size(477, 118);
-            this.listBoxControl1.TabIndex = 6;
-            this.listBoxControl1.DoubleClick += new System.EventHandler(this.listBoxControl1_DoubleClick);
-            // 
-            // labelControl2
-            // 
-            this.labelControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelControl2.Location = new System.Drawing.Point(1140, 50);
-            this.labelControl2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(69, 16);
-            this.labelControl2.TabIndex = 5;
-            this.labelControl2.Text = "Text length:";
-            // 
-            // nudTextLength
-            // 
-            this.nudTextLength.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.nudTextLength.Location = new System.Drawing.Point(1235, 47);
-            this.nudTextLength.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.nudTextLength.Maximum = new decimal(new int[] {
-            100000,
-            0,
-            0,
-            0});
-            this.nudTextLength.Name = "nudTextLength";
-            this.nudTextLength.Size = new System.Drawing.Size(82, 23);
-            this.nudTextLength.TabIndex = 4;
-            this.nudTextLength.Value = new decimal(new int[] {
-            50,
-            0,
-            0,
-            0});
-            this.nudTextLength.ValueChanged += new System.EventHandler(this.nudTextLength_ValueChanged);
             // 
             // sBtnAdd
             // 
             this.sBtnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.sBtnAdd.Location = new System.Drawing.Point(1235, 4);
+            this.sBtnAdd.Location = new System.Drawing.Point(919, 4);
             this.sBtnAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.sBtnAdd.Name = "sBtnAdd";
             this.sBtnAdd.Size = new System.Drawing.Size(85, 36);
@@ -158,7 +109,7 @@
             this.chklistItems.Location = new System.Drawing.Point(19, 44);
             this.chklistItems.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chklistItems.Name = "chklistItems";
-            this.chklistItems.Size = new System.Drawing.Size(813, 149);
+            this.chklistItems.Size = new System.Drawing.Size(497, 93);
             this.chklistItems.TabIndex = 2;
             this.chklistItems.ItemCheck += new DevExpress.XtraEditors.Controls.ItemCheckEventHandler(this.chklistItems_ItemCheck);
             // 
@@ -166,20 +117,20 @@
             // 
             this.textEdit1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textEdit1.Location = new System.Drawing.Point(180, 10);
+            this.textEdit1.Location = new System.Drawing.Point(337, 10);
             this.textEdit1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textEdit1.Name = "textEdit1";
-            this.textEdit1.Size = new System.Drawing.Size(1049, 22);
+            this.textEdit1.Size = new System.Drawing.Size(576, 22);
             this.textEdit1.TabIndex = 1;
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(19, 15);
+            this.labelControl1.Location = new System.Drawing.Point(19, 13);
             this.labelControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(154, 16);
+            this.labelControl1.Size = new System.Drawing.Size(307, 16);
             this.labelControl1.TabIndex = 0;
-            this.labelControl1.Text = "Text to plot along timeline:";
+            this.labelControl1.Text = "Filter messages (add empty for non filtering plotting):";
             // 
             // tabPane1
             // 
@@ -194,9 +145,9 @@
             this.tnpTimeDistribution,
             this.tabNavigationPage2,
             this.tabNavigationPage1});
-            this.tabPane1.RegularSize = new System.Drawing.Size(1324, 557);
+            this.tabPane1.RegularSize = new System.Drawing.Size(1008, 393);
             this.tabPane1.SelectedPage = this.tnpTimeDistribution;
-            this.tabPane1.Size = new System.Drawing.Size(1324, 557);
+            this.tabPane1.Size = new System.Drawing.Size(1008, 393);
             this.tabPane1.TabIndex = 1;
             // 
             // tabNavigationPage1
@@ -246,7 +197,7 @@
             this.tnpTimeDistribution.Caption = "Time Distribution";
             this.tnpTimeDistribution.Controls.Add(this.chartTimeDistribution);
             this.tnpTimeDistribution.Name = "tnpTimeDistribution";
-            this.tnpTimeDistribution.Size = new System.Drawing.Size(1324, 524);
+            this.tnpTimeDistribution.Size = new System.Drawing.Size(1008, 360);
             // 
             // chartTimeDistribution
             // 
@@ -258,23 +209,58 @@
             this.chartTimeDistribution.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
             sideBySideBarSeriesLabel6.LineVisibility = DevExpress.Utils.DefaultBoolean.True;
             this.chartTimeDistribution.SeriesTemplate.Label = sideBySideBarSeriesLabel6;
-            this.chartTimeDistribution.Size = new System.Drawing.Size(1324, 524);
+            this.chartTimeDistribution.Size = new System.Drawing.Size(1008, 360);
             this.chartTimeDistribution.TabIndex = 3;
+            // 
+            // xtraTabControl1
+            // 
+            this.xtraTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.xtraTabControl1.Location = new System.Drawing.Point(0, 0);
+            this.xtraTabControl1.Name = "xtraTabControl1";
+            this.xtraTabControl1.SelectedTabPage = this.xtpMessagesText;
+            this.xtraTabControl1.Size = new System.Drawing.Size(1015, 571);
+            this.xtraTabControl1.TabIndex = 2;
+            this.xtraTabControl1.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
+            this.xtpMessagesText,
+            this.xtpPieChart});
+            // 
+            // xtpMessagesText
+            // 
+            this.xtpMessagesText.Controls.Add(this.splitContainerControl1);
+            this.xtpMessagesText.Name = "xtpMessagesText";
+            this.xtpMessagesText.Size = new System.Drawing.Size(1008, 537);
+            this.xtpMessagesText.Text = "Messages Text";
+            // 
+            // xtpPieChart
+            // 
+            this.xtpPieChart.Controls.Add(this.scPies);
+            this.xtpPieChart.Name = "xtpPieChart";
+            this.xtpPieChart.Size = new System.Drawing.Size(1008, 537);
+            this.xtpPieChart.Text = "Pies";
+            // 
+            // scPies
+            // 
+            this.scPies.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scPies.Horizontal = false;
+            this.scPies.Location = new System.Drawing.Point(0, 0);
+            this.scPies.Name = "scPies";
+            this.scPies.Panel1.Text = "Panel1";
+            this.scPies.Panel2.Text = "Panel2";
+            this.scPies.Size = new System.Drawing.Size(1008, 537);
+            this.scPies.SplitterPosition = 191;
+            this.scPies.TabIndex = 0;
             // 
             // DataVisualizerUC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.splitContainerControl1);
-            this.Controls.Add(this.splitterControl1);
+            this.Controls.Add(this.xtraTabControl1);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "DataVisualizerUC";
-            this.Size = new System.Drawing.Size(1330, 757);
+            this.Size = new System.Drawing.Size(1015, 571);
             this.Load += new System.EventHandler(this.DataVisualizerUC_Load);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
             this.splitContainerControl1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudTextLength)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chklistItems)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tabPane1)).EndInit();
@@ -288,13 +274,17 @@
             this.tnpTimeDistribution.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(sideBySideBarSeriesLabel6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartTimeDistribution)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).EndInit();
+            this.xtraTabControl1.ResumeLayout(false);
+            this.xtpMessagesText.ResumeLayout(false);
+            this.xtpPieChart.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.scPies)).EndInit();
+            this.scPies.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private DevExpress.XtraEditors.SplitterControl splitterControl1;
         private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
         private DevExpress.XtraCharts.ChartControl chartControlOnOff;
         private DevExpress.XtraEditors.SimpleButton sBtnAdd;
@@ -305,10 +295,11 @@
         private DevExpress.XtraBars.Navigation.TabNavigationPage tabNavigationPage1;
         private DevExpress.XtraBars.Navigation.TabNavigationPage tabNavigationPage2;
         private DevExpress.XtraCharts.ChartControl chartControlFrequency;
-        private DevExpress.XtraEditors.ListBoxControl listBoxControl1;
-        private DevExpress.XtraEditors.LabelControl labelControl2;
-        private System.Windows.Forms.NumericUpDown nudTextLength;
         private DevExpress.XtraBars.Navigation.TabNavigationPage tnpTimeDistribution;
         private DevExpress.XtraCharts.ChartControl chartTimeDistribution;
+        private DevExpress.XtraTab.XtraTabControl xtraTabControl1;
+        private DevExpress.XtraTab.XtraTabPage xtpMessagesText;
+        private DevExpress.XtraTab.XtraTabPage xtpPieChart;
+        private DevExpress.XtraEditors.SplitContainerControl scPies;
     }
 }
