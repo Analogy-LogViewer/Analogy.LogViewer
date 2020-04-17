9d6d88212f12272e20f55819ac08edf60282a2f8
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-04-17 09:51:39 +0300

    update nugets

--- Analogy/UserControls/LocalLogFilesUC.cs
+++ Analogy/UserControls/LocalLogFilesUC.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
