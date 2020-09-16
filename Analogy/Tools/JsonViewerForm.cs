a66724f4cdf0be931e2f4d660b957119e15218b8
Lior Banai <liorbanai@gmail.com>
2020-09-16 19:49:47 +0300

    update nugets

---
+++ Analogy/Tools/JsonViewerForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Analogy.Tools
+{
+    public partial class JsonViewerForm : DevExpress.XtraEditors.XtraForm
+    {
+        public JsonViewerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
