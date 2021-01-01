4819b8af0dd1b6752d800fd526345312d35c0ed0
Lior Banai <liorbanai@gmail.com>
2021-01-01 19:04:50 +0200

    change initilization order of ui controls

--- Analogy/UserControls/UCLogs.cs
+++ Analogy/UserControls/UCLogs.cs
@@ -31,6 +31,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.XtraGrid;
 
 namespace Analogy
 {
@@ -144,8 +145,8 @@ namespace Analogy
         public UCLogs()
         {
             InitializeComponent();
-            WorkspaceManager.SetSerializationEnabled(logGrid,false);
-            WorkspaceManager.SetSerializationEnabled(gridControl, false);
+           // WorkspaceManager.SetSerializationEnabled(logGrid,false);
+           // WorkspaceManager.SetSerializationEnabled(gridControl, false);
 
             _simpleMode = Settings.SimpleMode;
             counts = new Dictionary<string, int>();
@@ -636,30 +637,34 @@ namespace Analogy
 
         private void LogGrid_CustomSummaryCalculate(object sender, CustomSummaryEventArgs e)
         {
-            if (e.SummaryProcess == CustomSummaryProcess.Start)
+            if (e.Item is GridSummaryItem item && item.FieldName.Equals(gridColumnLevel.FieldName))
             {
-                foreach (var key in Utils.LogLevels)
+                if (e.SummaryProcess == CustomSummaryProcess.Start)
                 {
-                    counts[key] = 0;
+                    foreach (var key in Utils.LogLevels)
+                    {
+                        counts[key] = 0;
+                    }
                 }
-            }
-            else if (e.SummaryProcess == CustomSummaryProcess.Calculate)
-            {
-                counts[(string)e.FieldValue] = counts[(string)e.FieldValue] + 1;
-            }
+                else if (e.SummaryProcess == CustomSummaryProcess.Calculate)
+                {
+                    counts[(string) e.FieldValue] = counts[(string) e.FieldValue] + 1;
+                }
 
-            else if (e.SummaryProcess == CustomSummaryProcess.Finalize)
-            {
-                bstaticTotalMessages.Caption = $"Total messages:{counts.Values.Sum()}. Errors:{counts["Error"]}. Warnings:{counts["Warning"]}. Critical:{counts["Critical"]}.";
-            }
-            //todo: add alerts
-            //if (Settings.PreDefinedQueries.Alerts.Any())
-            //{
-            //    var messages = rows.Select(r => (AnalogyLogMessage)r["Object"]).ToList();
-            //    alertCount = messages.Count(m =>
-            //        Settings.PreDefinedQueries.Alerts.Any(a => FilterCriteriaObject.MatchAlert(m, a)));
+                else if (e.SummaryProcess == CustomSummaryProcess.Finalize)
+                {
+                    bstaticTotalMessages.Caption =
+                        $"Total messages:{counts.Values.Sum()}. Errors:{counts["Error"]}. Warnings:{counts["Warning"]}. Critical:{counts["Critical"]}.";
+                }
+                //todo: add alerts
+                //if (Settings.PreDefinedQueries.Alerts.Any())
+                //{
+                //    var messages = rows.Select(r => (AnalogyLogMessage)r["Object"]).ToList();
+                //    alertCount = messages.Count(m =>
+                //        Settings.PreDefinedQueries.Alerts.Any(a => FilterCriteriaObject.MatchAlert(m, a)));
 
-            //}
+                //}
+            }
         }
 
         private void LogGrid_MouseDown(object sender, MouseEventArgs e)
@@ -960,6 +965,12 @@ namespace Analogy
         }
         private void LoadUISettings()
         {
+            gridControl.ForceInitialize();
+            if (File.Exists(Settings.LogGridFileName))
+            {
+                gridControl.MainView.RestoreLayoutFromXml(Settings.LogGridFileName);
+                gridControlBookmarkedMessages.MainView.RestoreLayoutFromXml(Settings.LogGridFileName);
+            }
             dockPanelBookmarks.Visibility = DockVisibility.Hidden;
             Utils.SetLogLevel(chkLstLogLevel);
             tmrNewData.Interval = (int)(Settings.RealTimeRefreshInterval * 1000);
@@ -967,18 +978,12 @@ namespace Analogy
             bBtnShare.Enabled =
                 FactoriesManager.Instance.Factories.SelectMany(f => f.ShareableFactories)
                     .SelectMany(fc => fc.Shareables).Any();
-                   
+                 
 
-            logGrid.Columns["Level"].SummaryItem.SummaryType = SummaryItemType.Custom;
-            logGrid.Columns["Level"].SummaryItem.FieldName = "Level";
             logGrid.OptionsSelection.MultiSelect = true;
             logGrid.OptionsSelection.MultiSelectMode = GridMultiSelectMode.RowSelect;
-            //logGrid.OptionsView.ShowFooter = true;
-            //Set up a summary on the "yourfieldname" column
-            //logGrid.Columns[0].SummaryItem.SummaryType = SummaryItemType.Custom;
-            ////logGrid.Columns[0].SummaryItem.FieldName = "id";
-
-            //logGrid.Columns[0].SummaryItem.DisplayFormat = "Rows: {0:n0}";
+           // logGrid.OptionsView.ShowFooter = true;
+           
             logGrid.OptionsFind.AlwaysVisible = Settings.IsBuiltInSearchPanelVisible;
             ceFilterPanelSearch.CheckState = Settings.BuiltInSearchPanelMode == BuiltInSearchPanelMode.Search ? CheckState.Checked : CheckState.Unchecked;
             ceFilterPanelFilter.CheckState = Settings.BuiltInSearchPanelMode == BuiltInSearchPanelMode.Filter ? CheckState.Checked : CheckState.Unchecked;
@@ -1002,12 +1007,6 @@ namespace Analogy
             txtbExclude.MaskBox.AutoCompleteSource = AutoCompleteSource.CustomSource;
             txtbExclude.MaskBox.AutoCompleteCustomSource = autoCompleteExclude;
 
-            gridControl.ForceInitialize();
-            if (File.Exists(Settings.LogGridFileName))
-            {
-                gridControl.MainView.RestoreLayoutFromXml(Settings.LogGridFileName);
-                gridControlBookmarkedMessages.MainView.RestoreLayoutFromXml(Settings.LogGridFileName);
-            }
             btswitchRefreshLog.Checked = true;
             LogGrid.BestFitColumns();
             btswitchMessageDetails.Checked = Settings.ShowMessageDetails;
