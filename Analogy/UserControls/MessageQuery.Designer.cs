13351091fbe48c52ed520a10e07a50bdb7cc8187
Lior Banai <liorbanai@gmail.com>
2021-03-06 19:03:22 +0200

    message query

---
+++ Analogy/UserControls/MessageQuery.Designer.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Analogy.UserControls
+{
+    partial class MessageQuery
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MessageQuery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "MessageQuery";
+            this.Size = new System.Drawing.Size(816, 361);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
