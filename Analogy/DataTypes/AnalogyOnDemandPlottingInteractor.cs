cf01d9b11002f01dfcc26dbb698352094dd8da4a
Lior Banai <liorbanai@gmail.com>
2021-07-15 22:43:10 +0300

    ..

---
+++ Analogy/DataTypes/AnalogyOnDemandPlottingInteractor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analogy.Interfaces;
+namespace Analogy.DataTypes
+{
+    public class AnalogyOnDemandPlottingInteractor: IAnalogyOnDemandPlottingInteractor
+    {
+        public void ShowPlot(Guid id, AnalogyOnDemandPlottingStartupType startupType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClosePlot(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSeriesFromPlot(Guid id, string seriesName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearAllData(Guid id, string seriesToClear)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
