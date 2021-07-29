7b8cbf8059a1b092d61063f2dbe81bc7c7e021a2
Lior Banai <liorbanai@gmail.com>
2021-07-29 15:15:45 +0300

    ..

---
+++ Analogy/UserControls/FilePlotterUC.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Analogy.UserControls
+{
+    partial class FilePlotterUC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFile = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.teFile = new DevExpress.XtraEditors.TextEdit();
+            this.sbtnLoad = new DevExpress.XtraEditors.SimpleButton();
+            this.sbtnBrowse = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.teFile.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFile
+            // 
+            this.lblFile.Location = new System.Drawing.Point(5, 27);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(68, 16);
+            this.lblFile.TabIndex = 0;
+            this.lblFile.Text = "File to load:";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(3, 3);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(341, 16);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "You can use this window to plot a tabular Data  from  a file.";
+            // 
+            // teFile
+            // 
+            this.teFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.teFile.Location = new System.Drawing.Point(79, 25);
+            this.teFile.Name = "teFile";
+            this.teFile.Size = new System.Drawing.Size(1010, 22);
+            this.teFile.TabIndex = 2;
+            // 
+            // sbtnLoad
+            // 
+            this.sbtnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.sbtnLoad.Location = new System.Drawing.Point(1163, 27);
+            this.sbtnLoad.Name = "sbtnLoad";
+            this.sbtnLoad.Size = new System.Drawing.Size(57, 20);
+            this.sbtnLoad.TabIndex = 3;
+            this.sbtnLoad.Text = "Load";
+            // 
+            // sbtnBrowse
+            // 
+            this.sbtnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.sbtnBrowse.Location = new System.Drawing.Point(1114, 26);
+            this.sbtnBrowse.Name = "sbtnBrowse";
+            this.sbtnBrowse.Size = new System.Drawing.Size(42, 20);
+            this.sbtnBrowse.TabIndex = 4;
+            this.sbtnBrowse.Text = "...";
+            // 
+            // FilePlotterUC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.sbtnBrowse);
+            this.Controls.Add(this.sbtnLoad);
+            this.Controls.Add(this.teFile);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.lblFile);
+            this.Name = "FilePlotterUC";
+            this.Size = new System.Drawing.Size(1222, 479);
+            this.Load += new System.EventHandler(this.GenericPlotterUC_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.teFile.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl lblFile;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.TextEdit teFile;
+        private DevExpress.XtraEditors.SimpleButton sbtnLoad;
+        private DevExpress.XtraEditors.SimpleButton sbtnBrowse;
+    }
+}
---
+++ Analogy/UserControls/FilePlotterUC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Analogy.UserControls
+{
+    public partial class FilePlotterUC : XtraUserControl
+    {
+        public FilePlotterUC()
+        {
+            InitializeComponent();
+        }
+
+        private void GenericPlotterUC_Load(object sender, EventArgs e)
+        {
+            SetupEventHandlers();
+        }
+
+        private void SetupEventHandlers()
+        {
+            sbtnBrowse.Click += (_, __) =>
+            {
+                using OpenFileDialog openFileDialog1 = new OpenFileDialog
+                {
+                    Title = @"Open Files",
+                    Multiselect = false
+                };
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    teFile.Text = openFileDialog1.FileName;
+                }
+            };
+        }
+    }
+}
