7dcfb7412b7e6fb455dbf83a416687f82b609063
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2021-09-11 11:23:27 +0300

    skeleton for user controls

---
+++ Analogy.CommonControls/UserControl1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Analogy.CommonControls
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
