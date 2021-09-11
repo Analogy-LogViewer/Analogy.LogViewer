8b1fc72f0c3f221527f8686798a325bfeabc42c4
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2021-09-11 11:57:33 +0300

    ..

---
+++ Analogy.CommonControls/DataTypes/FilterCriteriaUIOption.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analogy.CommonControls.DataTypes
+{
+    [Serializable]
+    public class FilterCriteriaUIOption
+    {
+        public string DisplayMember { get; set; }
+        public string ValueMember { get; set; }
+        public bool CheckMember { get; set; }
+
+
+        public FilterCriteriaUIOption(string displayMember, string valueMember, bool checkMember)
+        {
+            DisplayMember = displayMember;
+            ValueMember = valueMember;
+            CheckMember = checkMember;
+        }
+    }
+}
