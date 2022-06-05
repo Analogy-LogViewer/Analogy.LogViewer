6c0dc16954ecdacf20ccf0702d1461c2ca533f09
Lior Banai Kama <liorbanai@gmail.com>
2022-06-05 10:04:36 +0300

    what is new form updates

--- Analogy/Forms/WhatsNewForm.Designer.cs
+++ Analogy/Forms/WhatsNewForm.Designer.cs
@@ -55,9 +55,9 @@
             // fluentDesignFormContainer1
             // 
             this.fluentDesignFormContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.fluentDesignFormContainer1.Location = new System.Drawing.Point(176, 39);
+            this.fluentDesignFormContainer1.Location = new System.Drawing.Point(320, 39);
             this.fluentDesignFormContainer1.Name = "fluentDesignFormContainer1";
-            this.fluentDesignFormContainer1.Size = new System.Drawing.Size(870, 434);
+            this.fluentDesignFormContainer1.Size = new System.Drawing.Size(726, 434);
             this.fluentDesignFormContainer1.TabIndex = 0;
             // 
             // accordionControl1
@@ -85,7 +85,7 @@
             this.accordionControl1.Name = "accordionControl1";
             this.accordionControl1.OptionsMinimizing.AllowMinimizeMode = DevExpress.Utils.DefaultBoolean.False;
             this.accordionControl1.ScrollBarMode = DevExpress.XtraBars.Navigation.ScrollBarMode.Touch;
-            this.accordionControl1.Size = new System.Drawing.Size(176, 434);
+            this.accordionControl1.Size = new System.Drawing.Size(320, 434);
             this.accordionControl1.TabIndex = 1;
             this.accordionControl1.ViewType = DevExpress.XtraBars.Navigation.AccordionControlViewType.HamburgerMenu;
             // 
@@ -93,105 +93,105 @@
             // 
             this.e484.Name = "e484";
             this.e484.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e484.Text = "V4.8.4";
+            this.e484.Text = "V4.8.4 (Mar 13, 2022)";
             this.e484.Click += new System.EventHandler(this.e484_Click);
             // 
             // e480
             // 
             this.e480.Name = "e480";
             this.e480.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e480.Text = "V4.8.0";
+            this.e480.Text = "V4.8.0 (Nov 13, 2021)";
             this.e480.Click += new System.EventHandler(this.e480_Click);
             // 
             // e475
             // 
             this.e475.Name = "e475";
             this.e475.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e475.Text = "V4.7.5";
+            this.e475.Text = "V4.7.5 (Aug 07, 2021)";
             this.e475.Click += new System.EventHandler(this.e475_Click);
             // 
             // e474
             // 
             this.e474.Name = "e474";
             this.e474.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e474.Text = "V4.7.1 - 4.7.4";
+            this.e474.Text = "V4.7.1 - 4.7.4 (Jul 14, 2021)";
             this.e474.Click += new System.EventHandler(this.e474_Click);
             // 
             // e470
             // 
             this.e470.Name = "e470";
             this.e470.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e470.Text = "V4.7.0";
+            this.e470.Text = "V4.7.0 (May 01, 2021)";
             this.e470.Click += new System.EventHandler(this.e470_Click);
             // 
             // e460
             // 
             this.e460.Name = "e460";
             this.e460.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e460.Text = "V4.6.0";
+            this.e460.Text = "V4.6.0 (Feb 13, 2021)";
             this.e460.Click += new System.EventHandler(this.e460_Click);
             // 
             // e450
             // 
             this.e450.Name = "e450";
             this.e450.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e450.Text = "V4.5.0";
+            this.e450.Text = "V4.5.0 (Jan 23, 2021)";
             this.e450.Click += new System.EventHandler(this.e450_Click);
             // 
             // e441
             // 
             this.e441.Name = "e441";
             this.e441.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e441.Text = "V4.4.1";
+            this.e441.Text = "V4.4.1 (Jan 09, 2021)";
             this.e441.Click += new System.EventHandler(this.e441_Click);
             // 
             // e440
             // 
             this.e440.Name = "e440";
             this.e440.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e440.Text = "V4.4.0";
+            this.e440.Text = "V4.4.0 (Dec 12, 2020)";
             this.e440.Click += new System.EventHandler(this.e440_Click);
             // 
             // e432
             // 
             this.e432.Name = "e432";
             this.e432.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e432.Text = "V4.3.2";
+            this.e432.Text = "V4.3.2 (Nov 27, 2020)";
             this.e432.Click += new System.EventHandler(this.e432_Click);
             // 
             // e431
             // 
             this.e431.Name = "e431";
             this.e431.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e431.Text = "V4.3.1";
+            this.e431.Text = "V4.3.1 (Nov 14, 2020)";
             this.e431.Click += new System.EventHandler(this.e431_Click);
             // 
             // e430
             // 
             this.e430.Name = "e430";
             this.e430.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e430.Text = "V4.3.0";
+            this.e430.Text = "V4.3.0 (Oct 30, 2020)";
             this.e430.Click += new System.EventHandler(this.e430_Click);
             // 
             // e4210
             // 
             this.e4210.Name = "e4210";
             this.e4210.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e4210.Text = "V4.2.10";
+            this.e4210.Text = "V4.2.10 (Oct 17, 2020)";
             this.e4210.Click += new System.EventHandler(this.e4210_Click);
             // 
             // e429
             // 
             this.e429.Name = "e429";
             this.e429.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e429.Text = "V4.2.9";
+            this.e429.Text = "V4.2.9 (Sep 30, 2020)";
             this.e429.Click += new System.EventHandler(this.e429_Click);
             // 
             // e428
             // 
             this.e428.Name = "e428";
             this.e428.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e428.Text = "V4.2.8";
+            this.e428.Text = "V4.2.8 (Sep 16, 2020)";
             this.e428.Click += new System.EventHandler(this.e428_Click);
             // 
             // fluentDesignFormControl1
@@ -207,7 +207,7 @@
             // 
             this.e486.Name = "e486";
             this.e486.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.e486.Text = "V4.8.5 - 4.8.6";
+            this.e486.Text = "V4.8.5 - 4.8.6 (Apr 28 - June 05 2022)";
             this.e486.Click += new System.EventHandler(this.e486_Click);
             // 
             // WhatsNewForm
