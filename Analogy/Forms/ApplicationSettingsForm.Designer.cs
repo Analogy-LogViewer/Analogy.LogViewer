3ee33893607706ff781060bac7e43aa47e255500
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2022-07-08 16:50:43 +0300

    [Settings Form] simplifiy the UI

--- Analogy/Forms/ApplicationSettingsForm.Designer.cs
+++ Analogy/Forms/ApplicationSettingsForm.Designer.cs
@@ -32,21 +32,17 @@ namespace Analogy.Forms
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ApplicationSettingsForm));
             this.accordionControl1 = new DevExpress.XtraBars.Navigation.AccordionControl();
-            this.accordionControlElement2 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.applicationGeneralSettings = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.ApplicationUISettings = new DevExpress.XtraBars.Navigation.AccordionControlElement();
-            this.accordionControlElement1 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.messagesFiltering = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.MessagesLayout = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.colorSettings = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.MessagesColorHighlighting = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.predefinedQueries = new DevExpress.XtraBars.Navigation.AccordionControlElement();
-            this.DataProviders = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.DataProviderList = new DevExpress.XtraBars.Navigation.AccordionControlElement();
-            this.accordionControlElement4 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
-            this.accordionControlElement5 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.realDataProviderElement = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.FileExtensionsElement = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.DataProviderExternal = new DevExpress.XtraBars.Navigation.AccordionControlElement();
-            this.accordionControlSeparator1 = new DevExpress.XtraBars.Navigation.AccordionControlSeparator();
             this.shortcuts = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.Extensions = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.updates = new DevExpress.XtraBars.Navigation.AccordionControlElement();
@@ -63,31 +59,29 @@ namespace Analogy.Forms
             // 
             this.accordionControl1.Dock = System.Windows.Forms.DockStyle.Left;
             this.accordionControl1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
-            this.accordionControlElement2,
-            this.accordionControlElement1,
-            this.DataProviders,
+            this.applicationGeneralSettings,
+            this.ApplicationUISettings,
+            this.messagesFiltering,
+            this.MessagesLayout,
+            this.colorSettings,
+            this.MessagesColorHighlighting,
+            this.predefinedQueries,
+            this.DataProviderList,
+            this.realDataProviderElement,
+            this.FileExtensionsElement,
+            this.DataProviderExternal,
             this.shortcuts,
             this.Extensions,
             this.updates,
             this.debugging,
             this.Donations});
-            this.accordionControl1.Location = new System.Drawing.Point(0, 30);
+            this.accordionControl1.Location = new System.Drawing.Point(0, 39);
             this.accordionControl1.Margin = new System.Windows.Forms.Padding(4);
             this.accordionControl1.Name = "accordionControl1";
             this.accordionControl1.ScrollBarMode = DevExpress.XtraBars.Navigation.ScrollBarMode.Touch;
-            this.accordionControl1.Size = new System.Drawing.Size(325, 821);
+            this.accordionControl1.Size = new System.Drawing.Size(325, 812);
             this.accordionControl1.TabIndex = 1;
             this.accordionControl1.ViewType = DevExpress.XtraBars.Navigation.AccordionControlViewType.HamburgerMenu;
-            // 
-            // accordionControlElement2
-            // 
-            this.accordionControlElement2.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
-            this.applicationGeneralSettings,
-            this.ApplicationUISettings});
-            this.accordionControlElement2.Expanded = true;
-            this.accordionControlElement2.ImageOptions.Image = global::Analogy.Properties.Resources.Analogy_image_32x32;
-            this.accordionControlElement2.Name = "accordionControlElement2";
-            this.accordionControlElement2.Text = "Application Settings";
             // 
             // applicationGeneralSettings
             // 
@@ -104,19 +98,6 @@ namespace Analogy.Forms
             this.ApplicationUISettings.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
             this.ApplicationUISettings.Text = "UI Settings and Themes";
             this.ApplicationUISettings.Click += new System.EventHandler(this.applicationUISettings_Click);
-            // 
-            // accordionControlElement1
-            // 
-            this.accordionControlElement1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
-            this.messagesFiltering,
-            this.MessagesLayout,
-            this.colorSettings,
-            this.MessagesColorHighlighting,
-            this.predefinedQueries});
-            this.accordionControlElement1.Expanded = true;
-            this.accordionControlElement1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("accordionControlElement1.ImageOptions.Image")));
-            this.accordionControlElement1.Name = "accordionControlElement1";
-            this.accordionControlElement1.Text = "Messages Settings";
             // 
             // messagesFiltering
             // 
@@ -158,19 +139,6 @@ namespace Analogy.Forms
             this.predefinedQueries.Text = "Predefined queries";
             this.predefinedQueries.Click += new System.EventHandler(this.predefinedQueries_Click);
             // 
-            // DataProviders
-            // 
-            this.DataProviders.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
-            this.DataProviderList,
-            this.accordionControlElement4,
-            this.accordionControlElement5,
-            this.DataProviderExternal,
-            this.accordionControlSeparator1});
-            this.DataProviders.Expanded = true;
-            this.DataProviders.ImageOptions.Image = global::Analogy.Properties.Resources.Analogy_Icon2;
-            this.DataProviders.Name = "DataProviders";
-            this.DataProviders.Text = "Data Providers";
-            // 
             // DataProviderList
             // 
             this.DataProviderList.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("DataProviderList.ImageOptions.Image")));
@@ -179,21 +147,21 @@ namespace Analogy.Forms
             this.DataProviderList.Text = "List Of Providers";
             this.DataProviderList.Click += new System.EventHandler(this.DataProviderList_Click);
             // 
-            // accordionControlElement4
+            // realDataProviderElement
             // 
-            this.accordionControlElement4.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("accordionControlElement4.ImageOptions.Image")));
-            this.accordionControlElement4.Name = "accordionControlElement4";
-            this.accordionControlElement4.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.accordionControlElement4.Text = "Real Time Data Providers";
-            this.accordionControlElement4.Click += new System.EventHandler(this.accordionControlElement4_Click);
+            this.realDataProviderElement.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("accordionControlElement4.ImageOptions.Image")));
+            this.realDataProviderElement.Name = "realDataProviderElement";
+            this.realDataProviderElement.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.realDataProviderElement.Text = "Real Time Data Providers";
+            this.realDataProviderElement.Click += new System.EventHandler(this.accordionControlElement4_Click);
             // 
-            // accordionControlElement5
+            // FileExtensionsElement
             // 
-            this.accordionControlElement5.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("accordionControlElement5.ImageOptions.Image")));
-            this.accordionControlElement5.Name = "accordionControlElement5";
-            this.accordionControlElement5.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.accordionControlElement5.Text = "Default File Associations";
-            this.accordionControlElement5.Click += new System.EventHandler(this.accordionControlElement5_Click);
+            this.FileExtensionsElement.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("accordionControlElement5.ImageOptions.Image")));
+            this.FileExtensionsElement.Name = "FileExtensionsElement";
+            this.FileExtensionsElement.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.FileExtensionsElement.Text = "Default File Associations";
+            this.FileExtensionsElement.Click += new System.EventHandler(this.accordionControlElement5_Click);
             // 
             // DataProviderExternal
             // 
@@ -202,10 +170,6 @@ namespace Analogy.Forms
             this.DataProviderExternal.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
             this.DataProviderExternal.Text = "Additional Locations";
             this.DataProviderExternal.Click += new System.EventHandler(this.DataProviderExternal_Click);
-            // 
-            // accordionControlSeparator1
-            // 
-            this.accordionControlSeparator1.Name = "accordionControlSeparator1";
             // 
             // shortcuts
             // 
@@ -254,7 +218,7 @@ namespace Analogy.Forms
             this.bbtnReset});
             this.fluentDesignFormControl1.Location = new System.Drawing.Point(0, 0);
             this.fluentDesignFormControl1.Name = "fluentDesignFormControl1";
-            this.fluentDesignFormControl1.Size = new System.Drawing.Size(1198, 30);
+            this.fluentDesignFormControl1.Size = new System.Drawing.Size(1198, 39);
             this.fluentDesignFormControl1.TabIndex = 2;
             this.fluentDesignFormControl1.TabStop = false;
             this.fluentDesignFormControl1.TitleItemLinks.Add(this.bbtnReset);
@@ -273,10 +237,10 @@ namespace Analogy.Forms
             // fluentDesignFormContainer1
             // 
             this.fluentDesignFormContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.fluentDesignFormContainer1.Location = new System.Drawing.Point(325, 30);
+            this.fluentDesignFormContainer1.Location = new System.Drawing.Point(325, 39);
             this.fluentDesignFormContainer1.Margin = new System.Windows.Forms.Padding(4);
             this.fluentDesignFormContainer1.Name = "fluentDesignFormContainer1";
-            this.fluentDesignFormContainer1.Size = new System.Drawing.Size(873, 821);
+            this.fluentDesignFormContainer1.Size = new System.Drawing.Size(873, 812);
             this.fluentDesignFormContainer1.TabIndex = 0;
             // 
             // ApplicationSettingsForm
@@ -311,19 +275,15 @@ namespace Analogy.Forms
         private DevExpress.XtraBars.Navigation.AccordionControlElement shortcuts;
         private DevExpress.XtraBars.Navigation.AccordionControlElement MessagesColorHighlighting;
         private DevExpress.XtraBars.Navigation.AccordionControlElement predefinedQueries;
-        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement1;
         private DevExpress.XtraBars.Navigation.AccordionControlElement messagesFiltering;
         private DevExpress.XtraBars.Navigation.AccordionControlElement updates;
         private DevExpress.XtraBars.Navigation.AccordionControlElement debugging;
-        private DevExpress.XtraBars.Navigation.AccordionControlElement DataProviders;
-        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement2;
         private DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer fluentDesignFormContainer1;
         private DevExpress.XtraBars.Navigation.AccordionControlElement ApplicationUISettings;
         private DevExpress.XtraBars.Navigation.AccordionControlElement DataProviderList;
-        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement4;
-        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement5;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement realDataProviderElement;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement FileExtensionsElement;
         private DevExpress.XtraBars.Navigation.AccordionControlElement DataProviderExternal;
-        private DevExpress.XtraBars.Navigation.AccordionControlSeparator accordionControlSeparator1;
         private DevExpress.XtraBars.BarButtonItem bbtnReset;
         private DevExpress.XtraBars.Navigation.AccordionControlElement Donations;
     }
