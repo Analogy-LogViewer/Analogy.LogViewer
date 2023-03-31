5487e00b97f9cb39d13a515347b62f531d1e29e3
Lior Banai <liorbanai@gmail.com>
2023-03-31 11:58:17 +0300

    [EditFilePooling] Set center parent and anchor controls

--- Analogy/Forms/EditFilePooling.Designer.cs
+++ Analogy/Forms/EditFilePooling.Designer.cs
@@ -36,16 +36,19 @@
             // 
             // txtFilename
             // 
+            this.txtFilename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.txtFilename.Location = new System.Drawing.Point(14, 52);
-            this.txtFilename.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFilename.Margin = new System.Windows.Forms.Padding(4);
             this.txtFilename.Name = "txtFilename";
             this.txtFilename.Size = new System.Drawing.Size(904, 22);
             this.txtFilename.TabIndex = 0;
             // 
             // BtnOk
             // 
+            this.BtnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.BtnOk.Location = new System.Drawing.Point(832, 95);
-            this.BtnOk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnOk.Margin = new System.Windows.Forms.Padding(4);
             this.BtnOk.Name = "BtnOk";
             this.BtnOk.Size = new System.Drawing.Size(88, 28);
             this.BtnOk.TabIndex = 1;
@@ -55,7 +58,7 @@
             // lblHelp
             // 
             this.lblHelp.Location = new System.Drawing.Point(14, 23);
-            this.lblHelp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lblHelp.Margin = new System.Windows.Forms.Padding(4);
             this.lblHelp.Name = "lblHelp";
             this.lblHelp.Size = new System.Drawing.Size(232, 16);
             this.lblHelp.TabIndex = 2;
@@ -66,16 +69,17 @@
             this.AcceptButton = this.BtnOk;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(933, 138);
+            this.ClientSize = new System.Drawing.Size(933, 141);
             this.ControlBox = false;
             this.Controls.Add(this.lblHelp);
             this.Controls.Add(this.BtnOk);
             this.Controls.Add(this.txtFilename);
             this.IconOptions.ShowIcon = false;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "EditFilePooling";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Edit filename filter for pooling";
             ((System.ComponentModel.ISupportInitialize)(this.txtFilename.Properties)).EndInit();
             this.ResumeLayout(false);
