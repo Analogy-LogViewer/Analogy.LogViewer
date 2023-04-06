97b35b296210d39c2fd45a5bd824936ef0a62589
Lior Banai <liorbanai@gmail.com>
2023-04-06 20:18:00 +0300

    [Welcome screen] wip

--- Analogy/Forms/Welcome/WelcomeShareAndSupportUC.Designer.cs
+++ Analogy/Forms/Welcome/WelcomeShareAndSupportUC.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // WelcomeShareAndSupportUC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "WelcomeShareAndSupportUC";
+            this.Size = new System.Drawing.Size(1100, 600);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
